96ee9b81f00423b76a0eb4fb3b9363fcc02e870e
neeswebservices <neeswebservice@gmail.com>
2024-01-04 00:40:24 +0545

    changes

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Outlet } from "@remix-run/react";
+import { Link, Outlet } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -12,6 +12,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Recode Dev</h1>
+      <Link to="/main">Main</Link>
       <Outlet />
     </div>
   );
--- app/routes/main.tsx
+++ app/routes/main.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "@remix-run/react";
 export default function Page() {
   return (
     <div>
-      <h1>Main </h1>
+      {/* <h1>Main </h1> */}
       <Outlet key={"asdf"} />
     </div>
   );
